ab3b08f3f836c4e07f9d0fdaf86348bf89472617
Markus Mayer <code@mmayer.net>
2016-02-17 10:16:23 -0800

    Initial Commit

---
+++ tagler/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  tagler
+//
+//  Created by Markus Mayer on 2/17/16.
+//  Copyright © 2016 Markus Mayer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
