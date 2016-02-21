b552434503c7b23a902591b1b5480b8b5cdba186
Markus Mayer <code@mmayer.net>
2016-02-21 14:51:43 -0800

    main.m: Style tweaks

--- tagler/main.m
+++ tagler/main.m
@@ -8,7 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
